e3bcbecd2a29170153a6ac7b9645af74ba8b028b
Ruth Netser <54615090+rnetser@users.noreply.github.com>
2022-07-17 14:27:12 +0300

    Initial setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# openshift-cluster-management-python-wrapper
+Wrapper for openshift-cluster-management-python client
---
+++ setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name="openshift-cluster-management-python-wrapper",
+    license="apache-2.0",
+    keywords=["Openshift", "OCM"],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "colorlog",
+        "ocm-python-client",
+        "requests",
+    ],
+    python_requires=">=3.6",
+)
