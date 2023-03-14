441abfb1bc7948499b04924f6dccfbd9c393dbdc
Meni Yakove <myakove@gmail.com>
2023-03-14 15:38:04 +0200

    Add Dockerfile and cli entry point to run from container

--- scripts/cli.py
+++ scripts/cli.py
@@ -9,14 +9,22 @@ from ocm_python_wrapper.ocm_client import OCMPythonClient
 @click.command()
 @click.option("-a", "--addon", help="Addon name to install", required=True)
 @click.option(
+    "-e",
+    "--endpoint",
+    help="SSO endpoint url",
+    default="https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token",
+)
+@click.option(
+    "--action",
+    help="Action to perform",
+    required=True,
+    type=click.Choice(["install", "uninstall"]),
+)
+@click.option(
     "-p",
     "--parameters",
     multiple=True,
     help="Addon parameters for installation. each parameter pass as id=value",
-)
-@click.option("-i", "--install", help="Install the addon", default=False, is_flag=True)
-@click.option(
-    "-u", "--uninstall", help="Uninstall the addon", default=False, is_flag=True
 )
 @click.option(
     "-t",
@@ -27,37 +35,34 @@ from ocm_python_wrapper.ocm_client import OCMPythonClient
 )
 @click.option("-c", "--cluster", help="Cluster name", required=True)
 @click.option(
-    "-e",
     "--api-host",
     help="API host",
     default="production",
     type=click.Choice(["stage", "production"]),
 )
-def cli(addon, token, install, uninstall, parameters, api_host, cluster):
-    click.echo(f"Addon: {addon}")
-    click.echo(f"token: {token}")
-    click.echo(f"install: {install}")
-    click.echo(f"uninstall: {uninstall}")
-    click.echo(f"Parameters: {parameters}")
-    click.echo(f"api_host: {api_host}")
-    click.echo(f"cluster: {cluster}")
-
+def cli(addon, token, action, parameters, api_host, cluster, endpoint):
     _client = OCMPythonClient(
         token=token,
-        endpoint="https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token",
+        endpoint=endpoint,
         api_host=api_host,
         discard_unknown_keys=True,
     ).client
     cluster_addon = ClusterAddOn(client=_client, cluster_name=cluster, addon_name=addon)
+
+    install = action == "install"
     if install:
         _parameters = []
         for parameter in parameters:
+            if "=" not in parameter:
+                click.echo(f"parameters should be id=value, got {parameter}")
+                raise click.Abort()
+
             _id, _value = parameter.split("=")
             _parameters.append({"id": _id, "value": _value})
 
         cluster_addon.install_addon(parameters=_parameters)
 
-    elif uninstall:
+    else:
         cluster_addon.remove_addon()
 
 
