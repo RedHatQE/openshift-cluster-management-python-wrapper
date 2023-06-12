cbc442c881ecdeb8308a542ddc3d5a37cd0590aa
Ori Haran <oharan@redhat.com>
2023-06-12 14:24:23 +0300

    Fix logger

--- ocm_python_wrapper/cluster.py
+++ ocm_python_wrapper/cluster.py
@@ -398,9 +398,7 @@ class ClusterAddOn(Cluster):
             for rosa_sampler in self.addon_installation_instance_sampler(
                 func=rosa_cli.execute, wait_timeout=TIMEOUT_15MIN, command=_command
             ):
-                LOGGER.info(
-                    f"{rosa_sampler['out'].split_lines()}\n{rosa_sampler['err'].split_lines()}"
-                )
+                LOGGER.info(f"{rosa_sampler['out']}{rosa_sampler['err']}")
                 return rosa_sampler
 
         addon = AddOn(id=self.addon_name)
