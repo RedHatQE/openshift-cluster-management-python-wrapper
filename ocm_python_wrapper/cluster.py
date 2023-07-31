d0eb51663ae4ebea8cd266fdeff2bf39bf289e1a
Ori Haran <oharan@redhat.com>
2023-07-31 12:05:03 +0300

    Revert "dont use token temp"
    This reverts commit 684f299f61df11e7d8b8d8df14a072d31638b80d.

--- ocm_python_wrapper/cluster.py
+++ ocm_python_wrapper/cluster.py
@@ -595,8 +595,7 @@ class ClusterAddOn(Cluster):
                 }
             ],
         )
-        # secret_data_dict = {"auths": {"brew.registry.redhat.io": {"auth": brew_token}}}
-        secret_data_dict = {"auths": {"brew.registry.redhat.io": {}}}
+        secret_data_dict = {"auths": {"brew.registry.redhat.io": {"auth": brew_token}}}
         create_update_secret(
             secret_data_dict=secret_data_dict,
             name="pull-secret",  # pragma: allowlist secret
